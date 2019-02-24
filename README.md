fd1040276cdb3250362bd846bb987941dbc08f5e
karrak1 <g.sebestyen@fusionjatek.hu>
2019-02-24 09:58:53 +0100

    nations 9.0

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 <li><a href="https://github.com/karrak1/fusion_addons/tree/Fusion-9.0/pm_control">PM Kontroll</a></li>
 <li><a href="https://github.com/karrak1/fusion_addons/tree/Fusion-9.0/welcome_pm">Üdvözlő Üzenet</a></li>
 </ul>
+---
+<p>Profil:</p>
+<ul>
+<li><a href="https://github.com/karrak1/fusion_addons/tree/Fusion-9.0/nations">Felhasználó Országa</a></li>
+</ul>
---
+++ nations/includes/user_fields/user_nations_include.php
@@ -0,0 +1,64 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: user_nations_include.php
+| Author: karrak
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+if (!defined("IN_FUSION")) {
+    die("Access Denied");
+}
+
+$nation_set = 1; //0 = Just text; 1 = Icon + text; 2 = Just Icon.
+
+function show_flag($item) {
+	$items = strtr($item, " ", '_');
+    $icon = file_exists(IMAGES.'flags/flag_'.mb_strtolower($items).'.png') ? IMAGES.'flags/flag_'.mb_strtolower($items).'.png' : IMAGES.'flags/-.png';
+    $icname = translate_country_names($item);
+    return "<img class='img-thumbnail rounded mx-auto d-block' src=".$icon." title='".$icname."'>";
+}
+
+for ($i = 0; $i < count($locale['nations']); $i++) {
+    switch ($nation_set) {
+        case 1:
+            $value = show_flag($locale['nations'][$i])." ".translate_country_names($locale['nations'][$i]);
+            break;
+        case 2:
+            $value = show_flag($locale['nations'][$i]);
+            break;
+        default:
+            $value = translate_country_names($locale['nations'][$i]);
+    }
+    $locale['nations_array'][] = $value;
+}
+
+if ($profile_method == "input") {
+
+	$options = [
+        'inline'      => TRUE,
+        'allowclear'  => TRUE,
+        'placeholder' => $locale['uf_choose'],
+        'error_text'  => $locale['uf_nations_error'],
+        'options'     => $locale['nations_array']
+    ] + $options;
+
+    $user_fields = form_select('user_nations', $locale['uf_nations'], $field_value, $options);
+
+} elseif ($profile_method == "display") {
+    if ($field_value) {
+        $user_fields = [
+            'title' => $locale['uf_nations'],
+            'value' => $locale['nations_array'][$field_value]
+        ];
+    }
+}
