6258fb59ca001e27f5eb470eff5c880cd89c7b30
karrak1 <g.sebestyen@fusionjatek.hu>
2019-02-24 08:58:05 +0100

    add welcome pm
    9.0 verzio

---
+++ welcome_pm/README.md
@@ -0,0 +1,14 @@
+# PHP-Fusion_Addons
+<h1>PHP-Fusion 9.00 verzió kompatibilis.</h1>
+<p>Üdvözlő Üzenet panel</p>
+
+Üdvözlő PM Panel az új regisztrált tagoknak köszöntő privát üzenetet küld,
+miután regisztrált és bejelentkezett először az oldalra.
+Endedélyezés esetén, az Üzenőfalon is köszönti.
+
+Adminisztráció
+---
+  - Üdvözlő panel engedélyezhető vagy letiltható az infusions admin-ban.
+  - Megadható, az üzenetküldő Admin neve.
+  - Üdvözlő üzenet tárgya és az üzenet teljes mértékben testreszabható a locale fájlban
+  - Nem szükséges módosítani a PHP-Fusion core fájlokat.
---
+++ welcome_pm/infusions/welcome_pm_panel/locale/Hungarian/welcom.php
@@ -0,0 +1,38 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: welcome_pm_panel/locale/Hungarian/welcom.php
+| Author: karrak
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+
+$locale['WPM_000'] = "Üdvözlő PM Panel";
+$locale['WPM_001'] = "Egy üdvözlő PM-t küld az új tagoknak.";
+$locale['WPM_002'] = "Üdvözlő PM";
+
+$locale['WPM_005'] = "Üdvözöljük";
+$locale['WPM_006'] = "Helló,
+Üdvözöljük oldalunkon.
+Köszönjük, hogy regisztrált.
+
+Üdvözlettel
+Az oldal Vezetősége";
+$locale['WPM_007'] = "Üdvözlünk [USERNAME] köreinkben.";
+$locale['WPM_008'] = "Beállítások Módosítva.";
+
+$locale['WPM_009'] = "Üdvözlő PM Panel Beállítások";
+$locale['WPM_010'] = "Üdvözlő PM engedélyezése";
+$locale['WPM_011'] = "Üzenőfal bejegyzés engedélyezése";
+$locale['WPM_012'] = "Üdvözlő PM Panel Beállítások";
+$locale['WPM_013'] = "Küldő Admin";
+$locale['WPM_014'] = "Admin";
