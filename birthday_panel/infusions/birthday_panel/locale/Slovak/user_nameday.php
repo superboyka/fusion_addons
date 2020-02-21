1e02c6df7be1784e3d03a8016dc5dc4ce10d87f4
karrak1 <g.sebestyen@fusionjatek.hu>
2020-02-21 22:11:35 +0100

    add slovak nameday

---
+++ birthday_panel/infusions/birthday_panel/locale/Slovak/user_nameday.php
@@ -0,0 +1,7 @@
+<?php
+$locale['uf_nameday'] = "Névnap";
+$locale['uf_nameday_date'] = "%B %d";
+$locale['uf_nameday_desc'] = "A felhasználó névnapja";
+$locale['uf_greet'] = "</br>Sok Boldog Névnapot";
+$locale['uf_nameday_error'] = "Hiányos a Névnap profil kitöltése.";
+$locale['uf_nameday_day'] = [" van.", " volt.", " lesz."];
